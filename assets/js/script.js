e0400cc64aa9c04560a6ade1a3482d407fbae51e
Nubia Ascencio <nubiaascencioar@gmail.com>
2024-04-03 16:40:40 -0300

    modal, tooltip y posición navbar al hacer scroll arreglados

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,25 +7,31 @@ $(function() {
         var hash = this.hash;
 
         $('html, body').animate({
-            scrollTop: $(hash).offset().top
-        }, 800, function(){
-
-        window.location.hash = hash;
-        });
+            scrollTop: $(hash).offset().top - $("nav").height()
+        }, 900);
         }
     });
     //Modal
     $("#enviarFormulario").click(function (){
-        $("#modalFormulario").modal('show')
+        $("#modalFormulario").modal('show');
     })
 
     //Títulos en color azul al hacer dblclick"
     $('body').on('dblclick','h3',function(){
         $(this).css("color","blue")
-    })
+    });
+
+    $(window).scroll(function(){
+        if ($(this).scrollTop() > $("#carouselExampleInterval").offset().top) {
+            $(".navbar").addClass("bg-black");
+        } else {
+            $(".navbar").removeClass("bg-black ");
+        }
+    });
 })
 //Para que los tooltip funcionen
 var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
 var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
-  return new bootstrap.Tooltip(tooltipTriggerEl)
-})
+return new bootstrap.Tooltip(tooltipTriggerEl)
+});
+
